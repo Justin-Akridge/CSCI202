4ed2ec4e47f831150b2cc874e76571f79496cec3
Justin Akridge <Akridjus@gmail.com>
2024-03-29 11:30:05 -0400

    feat(algo): finished algorithm

--- M02/binary-to-decimal.cpp
+++ M02/binary-to-decimal.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[]) {
   auto binary_to_decimal = [](int bin, int weight) -> int {
     return bin * std::pow(2, weight);
   };
+
   std::string binary_number; 
   if (argc < 2) {
     std::cout << "Enter a binary number: ";
@@ -16,8 +17,7 @@ int main(int argc, char* argv[]) {
     std::string s(argv[1]);
     binary_number = s;
   }
-  //std::reverse(binary_number.begin(), binary_number.end());
-  // instead of reversing string we can stack items then pop
+
   std::stack<int> bin_stk;
   for (auto &ch : binary_number) {
     bin_stk.push(ch - '0');
