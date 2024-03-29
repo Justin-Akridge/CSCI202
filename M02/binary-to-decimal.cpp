c6e1f582c53db939e7464e4f8682377b55bd83a8
Justin Akridge <Akridjus@gmail.com>
2024-03-29 11:21:57 -0400

    feat(algo): working algo for binary to dec

--- M02/binary-to-decimal.cpp
+++ M02/binary-to-decimal.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
+#include <algorithm>
 #include <cmath>
+#include <string>
+#include <stack>
 
-
-void binary_to_decimal(long &binary_number, int &decimal, int& weight) {
-  int bit;
-  if (binary_number > 0) {
-    bit = binary_number % 10;
-    decimal = decimal + bit * static_cast<int>(std::pow(2.0, weight));
-    binary_number /= 10;
-    weight++;
-    binary_to_decimal(binary_number, decimal, weight);
-  }
-}
-
-int main(int argc, char* args[]) {
-  int decimal = 0;
-  int weight = 0;
-  long binary_number;
+int main(int argc, char* argv[]) {
+  auto binary_to_decimal = [](int bin, int weight) -> int {
+    return bin * std::pow(2, weight);
+  };
+  std::string binary_number; 
   if (argc < 2) {
     std::cout << "Enter a binary number: ";
     std::cin >> binary_number;
-    binary_to_decimal(binary_number, decimal, weight);
-  } 
-  std::cout << binary_number;
+  } else {
+    std::string s(argv[1]);
+    binary_number = s;
+  }
+  std::reverse(binary_number.begin(), binary_number.end());
+
+  int decimal = 0;
+  for (int i = 0; i < binary_number.size(); i++) {
+    decimal += binary_to_decimal(binary_number[i] - '0', i);
+  }
+  std::cout << decimal << '\n';
 }
