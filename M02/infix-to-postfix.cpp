bdcd0c60edf3ada9618e7c11751c6fb8a2252aea
Justin Akridge <Akridjus@gmail.com>
2024-03-29 14:37:12 -0400

    feat(updates): updates

--- M02/infix-to-postfix.cpp
+++ M02/infix-to-postfix.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <stack>
 
-int main() {
-  return 0;
+
+//infix to posfix expression
+//
+int main(int argc, char* argv[]) {
+  std::string infix;
+  if (argv < 2) {
+    std::cout << "Enter an expression: ";
+    std::cin >> infix;
+  } else {
+    string temp(argv[1]);
+    infix = temp;
+  }
+
+  std::string postifx;
+  std::stack<char> stk;
+
 }
