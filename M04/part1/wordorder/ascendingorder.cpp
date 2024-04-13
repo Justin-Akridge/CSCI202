9519d134b7edc8568af144c56b1e4f15782a5143
Justin Akridge <akridjus@gmail.com>
2024-04-13 11:34:39 -0400

    feat(sort): sorting algorithm fixed to remove punc

--- M04/part1/wordorder/ascendingorder.cpp
+++ M04/part1/wordorder/ascendingorder.cpp
@@ -3,25 +3,47 @@
 #include <string>
 #include <set>
 #include <algorithm>
+#include <cctype>
+#include <vector>
+
+class word_order {
+public:
+    void file_reader(std::ifstream& file);
+    void print();
+private:
+    std::set<std::string> words;
+};
+
+void word_order::file_reader(std::ifstream& file) {
+    std::string temp_word;
+    while (file >> temp_word) {
+        std::string word;
+        for (char c : temp_word) {
+            c = std::tolower(c);
+            if (isalpha(c)) {
+                word.push_back(c);
+            }
+        } 
+        words.insert(word);
+    }
+}
+
+void word_order::print() {
+    std::cout << "Words in ascending order:" << std::endl;
+    for (const auto &word : words) {
+        std::cout << word << " ";
+    }
+    std::cout << "\n";
+}
 
 int main() {
+    word_order reg;
     std::ifstream file("input.txt");
     if (!file.is_open()) {
         std::cerr << "Failed to open the file." << std::endl;
         return 1;
     }
-
-    std::set<std::string> words;
-    std::string word;
-    while (file >> word) {
-        words.insert(word);
-    }
+    reg.file_reader(file); 
     file.close();
-
-    std::cout << "Words in ascending order:" << std::endl;
-    for (const auto& w : words) {
-        std::cout << w << " ";
-    }
-    std::cout << '\n';
+    reg.print();
 }
-
