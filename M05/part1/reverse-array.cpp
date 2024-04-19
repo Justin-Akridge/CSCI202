37ca9365f497940133c9f944296275d7b1496e81
Justin Akridge <akridjus@gmail.com>
2024-04-19 18:08:10 -0400

    feat(refactor) to class

--- M05/part1/reverse-array.cpp
+++ M05/part1/reverse-array.cpp
@@ -1,40 +1,53 @@
 #include <iostream>
 #include <cassert>
-//Write a recursive function to implement the recursive algorithm of Exercise 16 (reversing the elements of an array between two indices). Also, write a program to test your function.
+#include <vector>
 
-void reverse(int list[], int len, int lower, int upper) {
-  assert(lower >= 0 && upper < len);
-  if (lower >= upper) return;
-  int temp = list[lower];
-  list[lower] = list[upper];
-  list[upper] = temp;
-  reverse(list, len, lower + 1, upper - 1);
-}
-
-void print_array(int list[], int len) {
-  for (int i = 0; i < len; i++) {
-    std::cout << list[i] << " ";
+class Reverse_array_type {
+public:
+  Reverse_array_type() { 
+    list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   }
-  std::cout << '\n';
-}
+  void reverse(int lower, int upper) {
+    assert(lower >= 0 && upper < list.size());
+    if (lower >= upper) return;
+    std::swap(list[lower], list[upper]);
+    reverse(lower + 1, upper - 1);
+  }
+
+  int size() const {
+    return list.size();
+  }
+
+  std::vector<int> get_list() const {
+    return list;
+  }
+
+  void print_array() {
+    for (const auto &num : list) {
+      std::cout << num << " ";
+    }
+    std::cout << '\n';
+  }
+private:
+  std::vector<int> list;
+};
 
 int main() {
-  int list[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  size_t len = sizeof(list) / sizeof(list[0]);
+  Reverse_array_type reg;
   int leftIndex, rightIndex;
   int test = 0;
   while (test < 3) {
     std::cout << "Enter start index to reverse (greater than 0): ";
     std::cin >> leftIndex;
-    if (leftIndex >= 0 && leftIndex < len) {
+    if (leftIndex >= 0 && leftIndex < reg.size()) {
       std::cout << "Enter right index to reverse (less than 9): ";
       std::cin >> rightIndex;
-      if (rightIndex < len) {
-	reverse(list, len, leftIndex, rightIndex);
-	print_array(list, len);
-	test++;
+      if (rightIndex < reg.size()) {
+        reg.reverse(leftIndex, rightIndex);
+        reg.print_array();
+        test++;
       } else {
-	std::cout << "Right index must be greater than or equal to zero and less than or equal to nine\n";
+        std::cout << "Right index must be greater than or equal to zero and less than or equal to nine\n";
       }
     } else {
       std::cout << "Left index must be greater than or equal to zero and less than or equal to nine\n";
